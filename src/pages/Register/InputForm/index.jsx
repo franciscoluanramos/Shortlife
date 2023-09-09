bd4a3c752c126d842e8ed97a8d28c5868ae3b021
Daniel-Alencar <danielalencar746@gmail.com>
2023-09-09 17:18:55 -0300

    Form dinâmico

--- src/pages/Register/InputForm/index.jsx
+++ src/pages/Register/InputForm/index.jsx
@@ -1,12 +1,14 @@
 import styles from './styles.module.css';
 
-export function InputForm({ title }) {
+export function InputForm({ title, value, setValue }) {
   return(
     <div className={styles.contentContainer}>
       <p className={styles.title}>{ title }</p>
       <input 
-        className={styles.input} 
+        className={styles.input}
         type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
       />
     </div>
   )
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 import { Quote } from '../../components/Quote';
 import { InputForm } from './InputForm';
 
+
 import styles from './styles.module.css';
 
 export function Register() {
+
+  const [oldYears, setOldYears] = useState();
+  const [sexOfBirth, setSexOfBirth] = useState();
+  const [country, setCountry] = useState();
+
+  const navigate = useNavigate();
+
   return(
     <Background>
       
@@ -30,13 +41,28 @@ export function Register() {
             Greetings to you!
           </h3>
 
-          <InputForm title={"How old are you?"}/>
-          <InputForm title={"What's your sex of birth?"}/>
-          <InputForm title={"Which country do you live in?"}/>
+          <InputForm 
+            title={"How old are you?"}
+            value={oldYears}
+            setValue={setOldYears}
+          />
+          <InputForm 
+            title={"What's your sex of birth?"}
+            value={sexOfBirth}
+            setValue={setSexOfBirth}
+          />
+          <InputForm 
+            title={"Which country do you live in?"}
+            value={country}
+            setValue={setCountry}
+          />
 
           <a
             className={styles.button} 
             href="#"
+            onClick={() => {
+              navigate("/result");
+            }}
           >
             START
           </a>
