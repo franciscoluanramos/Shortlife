e21abdb7c4063add56bb1e2d4ff5ed995259dd9c
Daniel-Alencar <danielalencar746@gmail.com>
2023-09-09 19:13:23 -0300

    Message

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -5,11 +5,6 @@ import backgroundImage from '../../assets/background.png';
 export function Background({ children }) {
   return(
     <div className={styles.contentBackground}>
-      <img 
-        className={styles.background}
-        src={backgroundImage} 
-        alt="background" 
-      />
       <div className={styles.contentContainer}>
         { children }
       </div>
