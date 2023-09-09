df74d68d1efb2c091e263e380711384b0c23de7e
Daniel-Alencar <danielalencar746@gmail.com>
2023-09-09 15:23:36 -0300

    Quote component

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -1,9 +1,14 @@
 import styles from './styles.module.css';
 
-export function Quote({ children, description, citation }) {
+export function Quote({ children, description, citation, strongCitation }) {
   return(
     <div className={styles.contentContainer}>
-      <p className={styles.description}>
+      <p className={styles.description}
+        style={strongCitation && {
+          fontWeight: 900,
+          fontSize: 35
+        }}
+      >
         { description }
       </p>
       <p className={styles.quoteCitation}>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -11,10 +11,11 @@ export function Register() {
           <Quote 
             description={"“...For what is your life? It is even a vapor that appeareth for a little time, and then vanisheth away. ”"}
             citation={"- James 4:14, Holy Bible"}
+            strongCitation
           />
           <Quote 
-            description={"“...For what is your life? It is even a vapor that appeareth for a little time, and then vanisheth away. ”"}
-            citation={"- James 4:14, Holy Bible"}
+            description={"Life's but a walking shadow, a poor player that struts and frets his hour upon the stage, and then is heard no more."}
+            citation={"- William Shakespear, The Tragedy of Macbeth"}
           />
         </div>
         <form
