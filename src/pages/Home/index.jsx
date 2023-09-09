0a039060a1658d37eb6d05937918bc5be0c95562
franciscoluanramos <francisco.ramos@discente.univasf.edu.br>
2023-09-09 16:10:25 -0300

    ainda estou na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import { ShareButton } from "../../components/ShareButton";
 import styles from "./styles.module.css"
@@ -5,17 +6,21 @@ import styles from "./styles.module.css"
 export function Home() {
     return(
         <div>
-            <Header/>
-            
-            <div className={styles.content}>
-                <div className={styles.title}>Time fleeth away </div>
-                <div className={styles.secondRow}>
-                    <div className={styles.title}>without delay</div>
-                    <div className={styles.author}>- English proverb</div>
+            <Background>
+                <Header/>
+                <div className={styles.content}>
+                    <div className={styles.hero}>
+                        <div className={styles.title}>Time fleeth away <br/> without delay </div>
+                        <div className={styles.author}>- English proverb</div>
+                        {/* <div className={styles.secondRow}>
+                            <div className={styles.title}>without delay</div>
+                            <div className={styles.author}>- English proverb</div>
+                        </div> */}
+                    </div>
+                    <div className={styles.descriptionText}>“Amor fati é uma expressão latina que significa 'amor ao destino', 'amor ao fado'. No estoicismo e na filosofia de Friedrich Nietzsche, significa ou trata-se de aceitação integral da vida e do destino humano mesmo em seus aspectos mais cruéis e dolorosos...”</div>
+                    <ShareButton/>
                 </div>
-                <div className={styles.descriptionText}>“Amor fati é uma expressão latina que significa 'amor ao destino', 'amor ao fado'. No estoicismo e na filosofia de Friedrich Nietzsche, significa ou trata-se de aceitação integral da vida e do destino humano mesmo em seus aspectos mais cruéis e dolorosos...”</div>
-                <ShareButton/>
-            </div>
+            </Background>
         </div>
     )
 }
