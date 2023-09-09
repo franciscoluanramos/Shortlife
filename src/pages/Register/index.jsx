a784840c74d3dc55af836034aeced195dd45256b
Daniel-Alencar <danielalencar746@gmail.com>
2023-09-09 13:12:00 -0300

    Background

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,9 @@
+import { Background } from '../../components/Background';
+import styles from './styles.module.css';
+
+export function Register() {
+  return(
+    <Background>
+    </Background>
+  )
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,12 @@
 import {Route, Routes as Switch} from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Register } from "../pages/Register";
 
 export const Routes = () => {
     return(
         <Switch>
             <Route path="/" Component={Home} />
-            
+            <Route path="/register" Component={Register} />
         </Switch>
     )
 }
