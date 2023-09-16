39c66bb3840574e7e93b69ce7db4664c0bf0a30f
Daniel-Alencar <danielalencar746@gmail.com>
2023-09-16 17:15:35 -0300

    Message

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css"
 
 export function Post(props) {
     return (
-        <div id={styles.post}>
+        <div ref={props.index} id={styles.post}>
             <div>{props.content}</div>
             <div>{props.author}</div>
         </div>
--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,53 +1,164 @@
-import styles from "./styles.module.css"
-import { Background } from "../../components/Background/index"
-import { Post } from "../../components/Post/Post"
-import { Header } from "../../components/Header/index"
+import { useEffect, useState, useRef } from "react";
+import styles from "./styles.module.css";
+
+import { Background } from "../../components/Background/index";
+import { Post } from "../../components/Post/Post";
+import { Header } from "../../components/Header/index";
+
 
 export function Quotes() {
+
+    const items = [
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        },
+        {
+            content: "Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over...",
+            author: "― Jonathan Nolan, Memento Mori"
+        }
+    ];
+
     return (
         <>
             <Background>
                 <Header/>
                 <div id={styles.contentContainer}>
-                    <div id={styles.quotes}>
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    /><Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    /><Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
-                    <Post 
-                        content="Time is an absurdity. An abstraction. The only thing that matters is this moment. This moment a million times over..."
-                        author="― Jonathan Nolan, Memento Mori"
-                    />
+                    <div style={{
+                        border: '1px solid red'
+                    }}>
+                        <div id={styles.quotes}>
+                            {
+                                items.map((item, index) => {
+                                    return(
+                                        <Post 
+                                            content={item.content}
+                                            author={item.author}
+                                        />
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </Background>
